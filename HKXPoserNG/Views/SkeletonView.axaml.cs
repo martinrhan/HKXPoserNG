177472cc4009d1adbcdc5e3b1e6490439b998e5c
martinrhan <martinrhan@126.com>
2025-11-01 10:52:56 +0800

    selecting a bone expands the treeview to locate it

--- HKXPoserNG/Views/SkeletonView.axaml.cs
+++ HKXPoserNG/Views/SkeletonView.axaml.cs
@@ -1,9 +1,11 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using Avalonia.VisualTree;
 using HKXPoserNG.ViewModels;
-using System.Reactive;
-using System.Reactive.Linq;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HKXPoserNG.Views;
 
@@ -11,5 +13,33 @@ public partial class SkeletonView : UserControl {
     public SkeletonView() {
         this.DataContext = Skeleton.Instance;
         InitializeComponent();
+        treeView.SelectionChanged += TreeView_SelectionChanged;
+    }
+
+    private void TreeView_SelectionChanged(object? sender, SelectionChangedEventArgs e) {
+        foreach (Bone bone in e.AddedItems.OfType<Bone>()) {
+            Stack<Bone> path = new();
+            Bone? b = bone;
+            do {
+                path.Push(b);
+                b = b.Parent;
+            }while (b != null);
+
+            Bone rootBone = path.Pop();
+            TreeViewItem rootTreeViewItem = treeView.GetVisualDescendants().OfType<TreeViewItem>().FirstOrDefault(tvi => tvi.DataContext == rootBone)!;
+
+            void Recursion(TreeViewItem treeViewItem, Stack<Bone> path) { 
+                if (path.Count == 0) return;
+                if (treeViewItem.IsExpanded) {
+                    Bone nextBone = path.Pop();
+                    TreeViewItem nextTreeViewItem = treeViewItem.GetVisualDescendants().OfType<TreeViewItem>().FirstOrDefault(tvi => tvi.DataContext == nextBone)!;
+                    Recursion(nextTreeViewItem, path);
+                } else {
+                    treeViewItem.IsExpanded = true;
+                    Dispatcher.UIThread.Post(() => Recursion(treeViewItem, path));
+                }
+            }
+            Recursion(rootTreeViewItem, path);
+        }
     }
 }
